8a7e3477280a41b5765466caefb91102f1a9c3b7
Dizzy <itsdizzy40@gmail.com>
2018-01-20 14:56:47 +0100

    A definite fix for api request errors

--- src/shared/normalizers/movie.js
+++ src/shared/normalizers/movie.js
@@ -1,8 +1,37 @@
 import { schema, normalize } from 'normalizr';
 
+const movieModel = {
+  _id: null,
+  imdb_id: null,
+  title: null,
+  year: null,
+  slug: null,
+  synopsis: null,
+  runtime: null,
+  country: null,
+  released: null,
+  trailer: null,
+  certification: null,
+  torrents: {},
+  rating: {
+    percentage: null,
+    watching: null,
+    votes: null
+  },
+  images: {
+    poster: null,
+    background: null
+  },
+  genres: []
+};
+
 // TODO: add torrents to schema
 export const movie = new schema.Entity('movies', {}, {
-  idAttribute: '_id'
+  idAttribute: '_id',
+  processStrategy: (value) => ({
+    ...movieModel,
+    ...value
+  })
 });
 export const movieList = [movie];
 
